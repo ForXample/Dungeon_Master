d0867ae24f59f9b49ebd485498f4f16be6d1a377
Zixi Liu <vic.zixi.liu@gmail.com>
2021-01-02 02:00:18 -0500

    【add】添加了自动生成地图的脚本，需要修改

---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RoomGenerator : MonoBehaviour
+{
+    public enum Direction { up, down, left, right };
+    public Direction direction;
+
+    [Header("Room Information")]
+    public GameObject roomPrefab;
+    public int roomNumber;
+    public Color startColor, endColor;
+
+    [Header("Position Controller")]
+    public Transform generatorPoint;
+    public float xOffset;
+    public float yOffset;
+    public List<GameObject> rooms = new List<GameObject>();
+
+
+    void Start()
+    {
+        for (int = 0; i < roomNumber, int++)
+        {
+            rooms.Add(Instantiate(roomPrefab, transform.position, Quaternion.identity));
+            ChangePointPosition();
+        }
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangePointPosition()
+    {
+        direction = (Direction)Random.Range(0,4);
+
+        switch(direction)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3 (0, yOffset, 0);
+                break;
+            case Direction.down:
+                generatorPoint.position += new Vector3 (0, -yOffset, 0);
+                break;
+            case Direction.left:
+                generatorPoint.position += new Vector3 (-xOffset, 0, 0);
+                break;
+            case Direction.right:
+                generatorPoint.position += new Vector3 (xOffset, 0, 0);
+                break;
+        }
+    }
+}
