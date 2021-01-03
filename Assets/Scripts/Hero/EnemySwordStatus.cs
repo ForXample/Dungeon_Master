75da7797a1fadc5b1314099e28f320466e38e6c8
Mike Feng <18wtf@queensu.ca>
2021-01-04 00:43:17 +0800

    [Fixing]重新增加了敌人和hero的基本属性，希望是最后一次修复

---
+++ Assets/Scripts/Hero/EnemySwordStatus.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySwordStatus : EnmeyUniversalStatus
+{
+    [Header("Sword类Enemy特有属性")]
+    public float example;
+}
